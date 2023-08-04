ebcfaaa6d53132430336ac74ca5d071b1002be31
JudithMathabathe <112932531+JudithMathabathe@users.noreply.github.com>
2023-08-04 09:24:14 +0200

    first push

---
+++ frontend/src/user/components/UserItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './UserItem.css'
+
+function UserItem(){
+    
+    return <h2>NewPlace Works!</h2>
+};
+
+export default UserItem;
---
+++ frontend/src/user/components/UsersList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './UsersList.css'
+
+function UsersList(){
+
+    return <h2>NewPlace Works!</h2>
+};
+
+export default UsersList;
